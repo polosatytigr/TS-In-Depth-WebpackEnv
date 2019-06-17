5e0a4bbe39bf8faf7e911e1eddb52866252871db
Yevhen Snihura <esnihura@gmail.com>
2019-06-17 19:05:20 +0300

    TS#10.Task-02

--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,41 @@ function showHello(divName: string, name: string) {
 
 // ========================================================================
 
+enum Category {
+  JavaScript,
+  CSS,
+  HTML,
+  TypeScript,
+  Angular
+}
 
-/* TASK: 01 */
+
 function getAllBooks(): Array<any> {
   let books:Array<any> = [
-    {title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true},
-    {title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false},
-    {title: 'CSS Secrets', author: 'Lea Verou', available: true},
-    {title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true}
+    {
+      title: 'Refactoring JavaScript',
+      author: 'Evan Burchard',
+      available: true,
+      category: Category.JavaScript
+    },
+    {
+      title: 'JavaScript Testing',
+      author: 'Liang Yuxian Eugene',
+      available: false,
+      category: Category.JavaScript
+    },
+    {
+      title: 'CSS Secrets',
+      author: 'Lea Verou',
+      available: true,
+      category: Category.CSS
+    },
+    {
+      title: 'Mastering JavaScript Object-Oriented Programming',
+      author: 'Andrea Chiarelli',
+      available: true,
+      category: Category.JavaScript
+    }
   ];
 
   return books;
@@ -24,7 +51,6 @@ function logFirstAvailable(books: Array<any>): void {
   const numberOfBooks: number = books.length;
   let firstAvailabelTitle: string = '';
 
-  // books.find((book: any))
   for (const book of books) {
     if (book.available) {
       firstAvailabelTitle = book.title;
@@ -36,15 +62,34 @@ function logFirstAvailable(books: Array<any>): void {
   console.log(`First available book title "${ firstAvailabelTitle }"`);
 }
 
+function getBookTitlesByCategory(category: Category): Array<string> {
+  const books = getAllBooks();
+  const titles: string[] = [];
 
+  for (const book of books) {
+    if (book.category === category) {
+      titles.push(book.title);
+    }
+  }
+
+  return titles;
+}
+
+function logBookTitles(titles: string[]): void {
+  titles.forEach(title => console.log(title));
+  /* equal to */
+  // for (const title of titles) {
+  //   console.log(title);
+  // }
+}
 
 
 // ========================================================================
 /* TASK: 01 */
-logFirstAvailable(getAllBooks());
+// logFirstAvailable(getAllBooks());
 
-
-
+/* TASK: 02 */
+logBookTitles(getBookTitlesByCategory(Category.JavaScript));
 
 
 
